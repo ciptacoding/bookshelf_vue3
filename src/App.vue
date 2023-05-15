bb254b950ad12b28208c07257f244ce0045c3dae
I Gusti Ngurah Cipta Dwipajaya <gusti.dwipajaya@digits.id>
2023-05-15 15:13:16 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,94 @@
+<template>
+	<div class="mt-8 p-4 md:px-28 lg:px-60 xl:px-72">
+		<section class="bg-white shadow-md rounded-md p-4 lg:px-28">
+			<header class="mb-4">
+				<h1 class="font-bold text-2xl text-center">Bookshelf Vue 3</h1>
+			</header>
+			<div class="mb-4">
+				<label for="addBook" class="block mb-1">Title</label>
+				<input type="text" id="addBook" v-model="title" class="rounded-md border-black w-full bg-gray-100 border border-solid py-1 px-2 focus:outline-cyan-500" placeholder="Input title">
+			</div>
+			<div class="mb-4">
+				<label for="addBook" class="block mb-1">Author</label>
+				<input type="text" id="addBook" v-model="author" class="rounded-md border-black w-full bg-gray-100 border border-solid py-1 px-2 focus:outline-cyan-500" placeholder="Input author">
+			</div>
+			<div class="mb-8">
+				<label for="addBook" class="block mb-1">Year</label>
+				<input type="text" id="addBook" v-model="year" class="rounded-md border-black w-full bg-gray-100 border border-solid py-1 px-2 focus:outline-cyan-500" placeholder="Input year">
+			</div>
+			<div class="flex justify-end">
+				<button @click="add"  class="bg-blue-500 px-8 py-1 text-white rounded-md shadow-md mb-4">Add Book</button>
+			</div>
+		</section>
+
+		<section class="bg-white shadow-md rounded-md p-4 mt-8">
+			<header class="mb-4">
+				<h1 class="font-bold text-2xl text-center">List Books</h1>
+			</header>
+			<div class="mt-4 mb-4 relative overflow-x-auto shadow-md rounded-sm sm:rounded-md">
+				<table class="w-full text-sm text-left text-gray-500">
+					<thead class="text-xs text-gray-700 uppercase bg-gray-200">
+						<tr class="border-b">
+							<th scope="col" class="px-6 py-3">No</th>
+							<th scope="col" class="px-6 py-3">Title</th>
+							<th scope="col" class="px-6 py-3">Author</th>
+							<th scope="col" class="px-6 py-3">Year</th>
+							<th scope="col" class="px-6 py-3">Actions</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr v-for="(book, index) in books" :key="index" class="bg-white border-b">
+							<td class="px-6 py-3">{{ index+1 }}</td>
+							<td class="px-6 py-3">{{ book.title }}</td>
+							<td class="px-6 py-3">{{ book.author }}</td>
+							<td class="px-6 py-3">{{ book.year }}</td>
+							<td class="px-6 py-3 flex items-center gap-2">
+								<button ><Icon icon="material-symbols:delete-outline" height="18" /></button>
+								<button ><Icon icon="mdi:clipboard-edit-outline" height="18" /></button>
+								<button ><Icon icon="octicon:checklist-24" height="18" /></button>
+							</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		</section>
+	</div>
+</template>
+
+<script>
+	import { Icon } from '@iconify/vue';
+	export default{
+		components: {Icon},
+
+		mounted(){
+			this.books = JSON.parse(localStorage.getItem('books')); //get data books from local storage
+		}, 
+
+		data(){
+			return{
+				title: '',
+				author: '',
+				year: '',
+				books: []
+			}
+		},
+
+		methods: {
+			add(){
+				this.books.push({
+					title: this.title,
+					author: this.author,
+					year: this.year,
+				});
+				this.title = ''
+				this.author = ''
+				this.year = ''
+				this.saveToLocalStorage();
+			},
+
+			saveToLocalStorage(){
+				localStorage.setItem('books', JSON.stringify(this.books))
+			},
+		},
+	};
+</script>
