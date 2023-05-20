9ac49c51c3d8241ce2e7c53e04d3130d54cf1c19
I Gusti Ngurah Cipta Dwipajaya <gusti.dwipajaya@digits.id>
2023-05-20 23:05:55 +0700

    Refactoring List book with new component

--- src/App.vue
+++ src/App.vue
@@ -5,54 +5,18 @@
 		</section>
 
 		<section class="bg-white shadow-md rounded-md p-4 mt-8">
-			<header class="mb-4">
-				<h1 class="font-bold text-2xl text-center">List Books</h1>
-			</header>
-			<div class="mt-4 mb-4 relative overflow-x-auto shadow-md rounded-sm sm:rounded-md">
-				<table class="w-full text-sm text-left text-gray-500">
-					<thead class="text-xs text-gray-700 uppercase bg-gray-200">
-						<tr class="border-b">
-							<th scope="col" class="px-6 py-3">No</th>
-							<th scope="col" class="px-6 py-3">Title</th>
-							<th scope="col" class="px-6 py-3">Author</th>
-							<th scope="col" class="px-6 py-3">Year</th>
-							<th scope="col" class="px-6 py-3">Actions</th>
-						</tr>
-					</thead>
-					<tbody>
-						<tr v-for="(book, index) in books" :key="index" class="bg-white border-b">
-							<td class="px-6 py-3">{{ index+1 }}</td>
-							<td class="px-6 py-3" v-if="book.isDone">
-								<del>{{ book.title }}</del>
-							</td>
-							<td class="px-6 py-3" v-else>{{book.title}}</td>
-							<td class="px-6 py-3">{{ book.author }}</td>
-							<td class="px-6 py-3">{{ book.year }}</td>
-							<td class="px-6 py-3 flex items-center gap-2">
-								<button :title="DeleteTitle" @click="deleteBook(index)" ><Icon icon="material-symbols:delete-outline" height="18" /></button>
-								<button :title="UpdateTitle"><Icon icon="mdi:clipboard-edit-outline" height="18" /></button>
-								<button :title="DoneTitle" @click="doneRead(index)"><Icon icon="octicon:checklist-24" height="18" /></button>
-							</td>
-						</tr>
-					</tbody>
-				</table>
-			</div>
+			<ListBooks :books="books" @delete-book="deleteBook" @done-read="doneRead"/>
 		</section>
 	</div>
 </template>
 
 <script>
-	import { Icon } from '@iconify/vue';
 	import FormInput from './components/FormInput.vue';
+	import ListBooks from './components/ListBooks.vue';
 	export default{
-		components: {Icon, FormInput},
+		components: {FormInput, ListBooks},
 		data(){
 			return{
-				// title
-				DeleteTitle: 'Delete Book',
-				UpdateTitle: 'Update Book',
-				DoneTitle: 'Is Done',
-
 				// v-model
 				title: '',
 				author: '',
